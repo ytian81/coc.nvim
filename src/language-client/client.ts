45dc898880440d7c8be261430de4010c0bced8b5
Heyward Fann <fannheyward@gmail.com>
2020-12-25 17:59:35 +0800

    feat(language-client): add InitializeParams.locale
    maybe we can use vim :language

--- src/language-client/client.ts
+++ src/language-client/client.ts
@@ -3649,6 +3649,7 @@ export abstract class BaseLanguageClient {
       initializationOptions: Is.func(initializationOptions) ? initializationOptions() : initializationOptions,
       trace: Trace.toString(this._trace),
       workspaceFolders: null,
+      locale: this.getLocale(),
       clientInfo: {
         name: 'coc.nvim',
         version: workspace.version
@@ -4238,6 +4239,27 @@ export abstract class BaseLanguageClient {
     return workspace.applyEdit(params.edit).then(value => {
       return { applied: value }
     })
+  }
+
+  private getLocale(): string {
+    // TODO: use vim :language or coc.preferences config?
+    interface NLS_CONFIG {
+      locale: string
+    }
+    const envValue = process.env['VSCODE_NLS_CONFIG']
+    if (envValue === undefined) {
+      return 'en'
+    }
+
+    let config: NLS_CONFIG | undefined = undefined
+    try {
+      config = JSON.parse(envValue)
+    } catch (err) {
+    }
+    if (config === undefined || typeof config.locale !== 'string') {
+      return 'en'
+    }
+    return config.locale
   }
 
   public logFailedRequest(type: MessageSignature, error: any): void {
