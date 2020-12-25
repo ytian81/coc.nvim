56349be61c2136948bd7db1dcf3d61e76a48a520
Heyward Fann <fannheyward@gmail.com>
2020-12-25 16:59:30 +0800

    feat(language-client): format

--- src/language-client/client.ts
+++ src/language-client/client.ts
@@ -130,8 +130,8 @@ interface ConnectionCloseHandler {
 }
 
 interface ConnectionOptions {
-    cancellationStrategy: CancellationStrategy
-    maxRestartCount?: number
+  cancellationStrategy: CancellationStrategy
+  maxRestartCount?: number
 }
 
 function createConnection(
@@ -724,7 +724,7 @@ export interface LanguageClientOptions {
   workspaceFolder?: WorkspaceFolder
   connectionOptions?: ConnectionOptions
   markdown?: {
-	  isTrusted?: boolean;
+    isTrusted?: boolean;
   }
 }
 
