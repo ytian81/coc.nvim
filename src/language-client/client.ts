fda19810cbce3f8661e2b87b847effc3a4991db4
Heyward Fann <fannheyward@gmail.com>
2020-12-29 10:50:32 +0800

    fix(language-client): WorkspaceFeature register

--- src/language-client/client.ts
+++ src/language-client/client.ts
@@ -1813,7 +1813,7 @@ abstract class WorkspaceFeature<RO, PR> implements DynamicFeature<RO> {
     this._registrations.set(data.id, { disposable: registration[0], provider: registration[1] })
   }
 
-  protected abstract registerLanguageProvider(options: RO): Disposable
+  protected abstract registerLanguageProvider(options: RO): [Disposable, PR]
 
   public unregister(id: string): void {
     const registration = this._registrations.get(id)
@@ -2337,7 +2337,7 @@ class WorkspaceSymbolFeature extends WorkspaceFeature<WorkspaceSymbolRegistratio
     })
   }
 
-  protected registerLanguageProvider(_options: WorkspaceSymbolRegistrationOptions): Disposable {
+  protected registerLanguageProvider(_options: WorkspaceSymbolRegistrationOptions): [Disposable, WorkspaceSymbolProvider] {
     const provider: WorkspaceSymbolProvider = {
       provideWorkspaceSymbols: (query, token) => {
         const client = this._client
@@ -2355,7 +2355,7 @@ class WorkspaceSymbolFeature extends WorkspaceFeature<WorkspaceSymbolRegistratio
           : provideWorkspaceSymbols(query, token)
       }
     }
-    return languages.registerWorkspaceSymbolProvider(provider)
+    return [languages.registerWorkspaceSymbolProvider(provider), provider]
   }
 }
 
